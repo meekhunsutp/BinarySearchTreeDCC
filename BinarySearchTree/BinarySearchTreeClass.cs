c5dfc5feeedb0467da98d44f9a2072c6a03218f4
unknown <meekhunsutp@gmail.com>
2020-10-30 17:13:52 -0500

    Created Binary Search Tree and Node class, created Add method

---
+++ BinarySearchTree/BinarySearchTreeClass.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class BinarySearchTreeClass
+    {
+        public Node RootNode;
+
+        public BinarySearchTreeClass()
+        {
+            RootNode = null;
+        }
+
+
+
+        public void Add(int data)
+        {
+            Node nodeToAdd = new Node(data);
+            if(RootNode == null)
+            {
+                RootNode = nodeToAdd;
+            }
+            Node focusNode = RootNode;
+            while(true)
+            {
+                if(nodeToAdd.Data < focusNode.Data)
+                {
+                    if(focusNode.LeftNode == null)
+                    {
+                        focusNode.LeftNode = nodeToAdd;
+                        return;
+                    }
+                    else
+                    {
+                        focusNode = focusNode.LeftNode;
+                    }
+                }
+                else
+                {
+                    if (focusNode.RightNode == null)
+                    {
+                        focusNode.RightNode = nodeToAdd;
+                        return;
+                    }
+                    else
+                    {
+                        focusNode = focusNode.RightNode;
+                    }
+                }
+            }
+
+        }
+
+        public void Search()
+        {
+
+        }
+    }
+}
